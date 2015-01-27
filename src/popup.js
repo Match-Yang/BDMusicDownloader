edd55c71eb80708969c787aab0b9aa558e2d88b1
armsword <root@linuxer.me>
2015-01-27 23:49:05 +0800

    百度无损音乐下载插件

--- src/popup.js
+++ src/popup.js
@@ -1,4 +1,6 @@
+//var temp;
 
+// 异步调用
 function readHTML(new_url){
     var value;
     value = $.ajax({
@@ -11,32 +13,48 @@ function readHTML(new_url){
     },
     success : function(data)
     {
-        value = data;
-        parseHtml(value);
+        temp = data;
+        parseHtml(temp);
     }
     });
-    
+    //return temp;
 }
 
-
+// 解析JSON，然后使用rate大小做判断
 function parseHtml(html)
 {
     var name = html.data.songList[0].songName;
     var author = html.data.songList[0].artistName;
     var link = html.data.songList[0].showLink;
-    x = document.getElementById("name");
-    x.innerHTML = name;
-    y = document.getElementById("author");
-    y.innerHTML = author;
-    z = document.getElementById("down");
-    z.innerHTML = link; 
+    var rate = html.data.songList[0].rate;
+    //alert(name);
+    if(rate < 320)
+    {
+        standard = document.getElementById("name");
+        standard.innerHTML = name;
+        standard = document.getElementById("standard");
+        standard.innerHTML = "标准品质"+link;
+    }
+    else if(rate > 320)
+    {
+        ultimate = document.getElementById("ultimate");
+        ultimate.innerHTML = "无损品质"+link;
+    }
+    else
+    {
+        high = document.getElementById("high");
+        high.innerHTML = "超高品质"+link;
+    }
 }
 
 function getMusicAddr(id)
 {
-    
-
-
+    url_standard = "http://music.baidu.com/data/music/fmlink?songIds="+id+"&type=mp3&rate=192"; 
+    readHTML(url_standard);
+    url_high = "http://music.baidu.com/data/music/fmlink?songIds="+id+"&type=mp3&rate=320";
+    readHTML(url_high);
+    url_ultimate = "http://music.baidu.com/data/music/fmlink?songIds="+id+"&type=flac";
+    readHTML(url_ultimate);
 }
 
 function bdMusicDownload()
